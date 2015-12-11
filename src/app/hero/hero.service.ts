814f9b99678b4c95237f692ce953a0223f96036c
Phong Huynh <phuynh@architech.ca>
2015-12-11 16:15:32 -0500

    Fix tabs mixed in with spaces

--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -3,32 +3,31 @@ import {AngularFire, FirebaseArray} from 'firebase/angularfire';
 
 export class HeroService {
   heroes: FirebaseArray;
-  
+
   constructor() {
     var data = new AngularFire(new Firebase('https://heroes-angular2.firebaseio.com/'));
-		this.heroes = data.asArray();
+    this.heroes = data.asArray();
   }
-  
+
   getHeroes() {
     return this.heroes.list;
   }
-  
+
   addHero(hero: HeroModel) {
     this.heroes.add(hero);
   }
-  
+
   removeHero(hero: HeroModel){
-		this.heroes.remove(hero);
-	}
-	
-	updateHero(hero: HeroModel){
-		this.heroes.save(hero);
-	}
-	
-	removeAll(){
-		for( var i = this.heroes.length - 1 ; i >= 0 ; i -- )
-		{
-			this.heroes.remove( this.heroes[i] );
-		}
-	}
+    this.heroes.remove(hero);
+  }
+
+  updateHero(hero: HeroModel){
+    this.heroes.save(hero);
+  }
+
+  removeAll(){
+    for( var i = this.heroes.length - 1 ; i >= 0 ; i -- ) {
+      this.heroes.remove( this.heroes[i] );
+    }
+  }
 }
